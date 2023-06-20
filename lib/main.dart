6b6e2bc2deaff0c51e819e45bfb215b890c474b6
VivekYadavInductus <vivek.yadav@inductusgroup.com>
2023-06-20 16:12:44 +0530

    Shown Stripe Pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:http/http.dart' as http;
 import 'package:inductus_jobs/services/paymentServices/payment_services.dart';
 import 'app/constant.dart';
 
@@ -73,62 +70,4 @@ class _StripeScreenState extends State<StripeScreen> {
       ),
     );
   }
-
-Future<void> payment() async {
-  try {
-    paymentIntent = await createPaymentIntent('1000', 'INR');
-
-    var gpay = const PaymentSheetGooglePay(
-        merchantCountryCode: "IN", currencyCode: "INR", testEnv: true);
-
-    //STEP 2: Initialize Payment Sheet
-    await Stripe.instance
-        .initPaymentSheet(
-            paymentSheetParameters: SetupPaymentSheetParameters(
-                paymentIntentClientSecret: paymentIntent![
-                    'client_secret'], //Gotten from payment intent
-                style: ThemeMode.light,
-                merchantDisplayName: 'Inductus',
-                googlePay: gpay))
-        .then((value) {});
-
-    //STEP 3: Display Payment sheet
-    displayPaymentSheet();
-  } catch (err) {
-    print(err);
-  }
-}
-
-// ** Display Sheet Function
-displayPaymentSheet() async {
-  try {
-    await Stripe.instance.presentPaymentSheet().then((value) {
-      print("Payment Successfully");
-    });
-  } catch (e) {
-    print('$e');
-  }
-}
-
-createPaymentIntent(String amount, String currency) async {
-  try {
-    Map<String, dynamic> body = {
-      'amount': amount,
-      'currency': currency,
-    };
-
-    var response = await http.post(
-      Uri.parse('https://api.stripe.com/v1/payment_intents'),
-      headers: {
-        'Authorization':
-            'Bearer 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',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: body,
-    );
-    return json.decode(response.body);
-  } catch (err) {
-    throw Exception(err);
-  }
-}
 }
