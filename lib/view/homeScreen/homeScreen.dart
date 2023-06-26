1b6be899ea9cb9b617c8af949a567c57b4c06856
VivekYadavInductus <vivek.yadav@inductusgroup.com>
2023-06-26 18:53:06 +0530

    Shown Stripe Pop, Login Screen Design Done, Registration Screen Complete,Otp Screen Done

--- lib/view/homeScreen/homeScreen.dart
+++ lib/view/homeScreen/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,24 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(elevation: 0.0, backgroundColor: Colors.white),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearchBar());
+              print("Hello Search");
+            },
+            icon: const FaIcon(FontAwesomeIcons.magnifyingGlass),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.bell),
+          )
+        ],
+      ),
+      resizeToAvoidBottomInset: false,
       drawer: Drawer(
         backgroundColor: Colors.blueGrey,
         shape: const RoundedRectangleBorder(
@@ -42,13 +60,81 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      resizeToAvoidBottomInset: false,
       body: LayoutBuilder(builder: (context, BoxConstraints constraints) {
         return const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+            
+          ],
         );
       }),
     ));
+  }
+}
+
+//**  Search Bar.....  //
+class CustomSearchBar extends SearchDelegate {
+  List<String> searchResults = [
+    'Brazil',
+    'China',
+    'India',
+    'America',
+    'Russia',
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    IconButton(
+        onPressed: () {
+          if (query.isEmpty) {
+            close(context, null);
+          } else {
+            query = '';
+          }
+        },
+        icon: const FaIcon(FontAwesomeIcons.xmark));
+    return null;
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    IconButton(
+        onPressed: () => close(context, null),
+        icon: const FaIcon(FontAwesomeIcons.arrowLeft));
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+        child: Text(
+          query,
+          style: const TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestion = searchResults.where((element) {
+      final result = element.toLowerCase();
+      final input = query.toLowerCase();
+      return result.contains(input);
+    }).toList();
+    return ListView.builder(
+        itemCount: suggestion.length,
+        itemBuilder: (context, index) {
+          final suggestionCountry = suggestion[index];
+          return ListTile(
+            onTap: () {
+              query = suggestionCountry;
+              showResults(context);
+            },
+            title: Text(suggestionCountry),
+          );
+        });
   }
 }
