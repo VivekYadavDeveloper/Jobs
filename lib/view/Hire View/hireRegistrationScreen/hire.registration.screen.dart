603e28cbbca5a92a56ab7e90f23e3c49861ae564
VivekYadavInductus <vivek.yadav@inductusgroup.com>
2023-07-26 18:28:40 +0530

    Edit Screen / Fix Next Button/ Detail Screen/Apply Screen/Route Done/Splash Screen/ Hire Screens

--- lib/view/Hire View/hireRegistrationScreen/hire.registration.screen.dart
+++ lib/view/Hire View/hireRegistrationScreen/hire.registration.screen.dart
@@ -32,136 +32,142 @@ class _HireRegistrationScreenState extends State<HireRegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: SingleChildScrollView(
-        child: LayoutBuilder(
-          builder: (context, BoxConstraints constraints) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CustomBackButton(route: "/hireLoginScreen"),
-                hireWelcomeTextRegistration(),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  35.0, 10.0, 35.0, 2.0),
-                              child: CustomTextField.customTextField(
-                                  textEditingController: hireUserNameController,
-                                  helperText: 'Enter User Name',
-                                  validator: (val) =>
-                                      val!.isEmpty ? 'Enter an Username' : null,
-                                  textInputType: TextInputType.text),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  35.0, 10.0, 35.0, 2.0),
-                              child: CustomTextField.customTextField(
-                                  textEditingController:
-                                      hireUserEmailController,
-                                  helperText: 'User Email',
-                                  validator: (val) =>
-                                      !RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
-                                              .hasMatch(val!)
-                                          ? 'Enter an email'
-                                          : null,
-                                  textInputType: TextInputType.emailAddress),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  35.0, 10.0, 35.0, 2.0),
-                              child: CustomTextField.customTextField(
-                                  textEditingController:
-                                      hirePhoneNumberController,
-                                  helperText: 'Enter Phone Number',
-                                  textInputType: TextInputType.phone,
-                                  validator: (value) {
-                                    String patttern =
-                                        r'(^(?:[+0]9)?[0-9]{10,12}$)';
-                                    RegExp regExp = RegExp(patttern);
-                                    if (value?.length == 0) {
-                                      return 'Please enter mobile number';
-                                    } else if (!regExp.hasMatch(value!)) {
-                                      return 'Please enter valid mobile number';
-                                    }
-                                    return null;
-                                  }),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  35.0, 10.0, 35.0, 2.0),
-                              child: CustomTextField.customTextField(
-                                  textEditingController: hireUserPassController,
-                                  helperText: 'Enter Password',
-                                  validator: (val) => val!.isEmpty
-                                      ? 'Please Fill Password'
-                                      : null,
-                                  textInputType: TextInputType.text),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  35.0, 10.0, 35.0, 2.0),
-                              child: CustomTextField.customTextField(
-                                  textEditingController: hireUserPassController,
-                                  helperText: 'Confirm Password',
-                                  validator: (val) => val!.isEmpty
-                                      ? 'Please Fill Confirm Password'
-                                      : null,
-                                  textInputType: TextInputType.text),
-                            ),
-                          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: LayoutBuilder(
+            builder: (context, BoxConstraints constraints) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const CustomBackButton(route: "/hireLoginScreen"),
+                  hireWelcomeTextRegistration(),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    35.0, 10.0, 35.0, 2.0),
+                                child: CustomTextField.customTextField(
+                                    textEditingController:
+                                        hireUserNameController,
+                                    helperText: 'Enter User Name',
+                                    validator: (val) => val!.isEmpty
+                                        ? 'Enter an Username'
+                                        : null,
+                                    textInputType: TextInputType.text),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    35.0, 10.0, 35.0, 2.0),
+                                child: CustomTextField.customTextField(
+                                    textEditingController:
+                                        hireUserEmailController,
+                                    helperText: 'User Email',
+                                    validator: (val) =>
+                                        !RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+                                                .hasMatch(val!)
+                                            ? 'Enter an email'
+                                            : null,
+                                    textInputType: TextInputType.emailAddress),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    35.0, 10.0, 35.0, 2.0),
+                                child: CustomTextField.customTextField(
+                                    textEditingController:
+                                        hirePhoneNumberController,
+                                    helperText: 'Enter Phone Number',
+                                    textInputType: TextInputType.phone,
+                                    validator: (value) {
+                                      String patttern =
+                                          r'(^(?:[+0]9)?[0-9]{10,12}$)';
+                                      RegExp regExp = RegExp(patttern);
+                                      if (value?.length == 0) {
+                                        return 'Please enter mobile number';
+                                      } else if (!regExp.hasMatch(value!)) {
+                                        return 'Please enter valid mobile number';
+                                      }
+                                      return null;
+                                    }),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    35.0, 10.0, 35.0, 2.0),
+                                child: CustomTextField.customTextField(
+                                    textEditingController:
+                                        hireUserPassController,
+                                    helperText: 'Enter Password',
+                                    validator: (val) => val!.isEmpty
+                                        ? 'Please Fill Password'
+                                        : null,
+                                    textInputType: TextInputType.text),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    35.0, 10.0, 35.0, 2.0),
+                                child: CustomTextField.customTextField(
+                                    textEditingController:
+                                        hireUserPassController,
+                                    helperText: 'Confirm Password',
+                                    validator: (val) => val!.isEmpty
+                                        ? 'Please Fill Confirm Password'
+                                        : null,
+                                    textInputType: TextInputType.text),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      /** Checkbox Widget **/
-                      Row(children: <Widget>[
-                        Checkbox(
-                          activeColor: AppColors.activeColor,
-                          hoverColor: AppColors.whiteColor,
-                          value: checkValue,
-                          onChanged: (bool? newValue) {
-                            setState(() {
-                              checkValue = newValue!;
-                            });
-                          },
-                        ),
-                        Text(
-                          'I agree to the privacy and policy: ',
-                          style: TextStyle(
-                              fontSize: 17.0.sp, color: AppColors.whiteColor),
+                        /** Checkbox Widget **/
+                        Row(children: <Widget>[
+                          Checkbox(
+                            activeColor: AppColors.activeColor,
+                            hoverColor: AppColors.whiteColor,
+                            value: checkValue,
+                            onChanged: (bool? newValue) {
+                              setState(() {
+                                checkValue = newValue!;
+                              });
+                            },
+                          ),
+                          Text(
+                            'I agree to the privacy and policy: ',
+                            style: TextStyle(
+                                fontSize: 17.0.sp, color: AppColors.whiteColor),
+                          )
+                        ]),
+                        SizedBox(width: 10.w),
+                        SizedBox(
+                          height: 40.h,
+                          width: 150.w,
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          AppColors.activeColor)),
+                              onPressed: () {},
+                              child: Text(
+                                "SIGN IN",
+                                style: GoogleFonts.roboto(
+                                    textStyle: TextStyle(
+                                        fontSize: 15.sp,
+                                        color: AppColors.whiteColor,
+                                        fontWeight: FontWeight.bold)),
+                              )),
                         )
-                      ]),
-                      SizedBox(width: 10.w),
-                      SizedBox(
-                        height: 40.h,
-                        width: 150.w,
-                        child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        AppColors.activeColor)),
-                            onPressed: () {},
-                            child: Text(
-                              "SIGN IN",
-                              style: GoogleFonts.roboto(
-                                  textStyle: TextStyle(
-                                      fontSize: 15.sp,
-                                      color: AppColors.whiteColor,
-                                      fontWeight: FontWeight.bold)),
-                            )),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            );
-          },
+                      ],
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
